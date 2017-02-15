0d04af3374e331608e6ffb5619b1b008b63b67bc
kack <kack-kun@hotmail.com>
2017-02-15 01:05:32 -0300

    Update mshome.py

--- mshome.py
+++ mshome.py
@@ -28,7 +28,7 @@ def clean():
 		try:
 			os.system('clear')
 		except:
-			print "Erro ao Limpar"
+			print "Erro ao Limpar!"
 
 def progress(count, total):
 	bar_len = 40
@@ -46,7 +46,7 @@ def download(download_name,download_url,download_limit = False,download_current 
 		try:
 			download_urls = url(download_url)
 		except:
-			print 'Falha ao baixar a Imagem'
+			print 'Falha ao baixar a Imagem!'
 		else:
 			pathh = "{0}".format(os.path.join(os.path.expandvars("%userprofile%"),"Desktop", download_name))
 			if not os.path.exists(pathh):
@@ -63,7 +63,7 @@ def download(download_name,download_url,download_limit = False,download_current 
 			try: 
 				file = open(join(pathh, nname),'wb')
 			except:
-				print 'Falha ao criar a Pasta'
+				print 'Falha ao criar a Pasta!'
 			else:
 				file.write(resource.read())
 				file.close()
@@ -72,7 +72,7 @@ def download(download_name,download_url,download_limit = False,download_current 
 	else:
 		progress(downl, download_limit)
 		print "\nFeito!\n"
-		option = raw_input("Continuar? [s our n] ")
+		option = raw_input("Continuar?! [s our n] ")
 	
 		if option is "y":
 			clean()
@@ -131,13 +131,13 @@ def intro():
 			if realase[int(option)]:
 				download(realase[int(option)][0],realase[int(option)][1])
 			else:
-				print 'Opcao nao existe'
+				print 'Opcao nao existe!'
 		else:
-			print 'Em breve, que sabe adiciono mais opcoes'
+			print 'Em breve, quem sabe adiciono mais opcoes!'
 	else:
 		intro()
 
 if chkdir(path)[0]:
 	intro()
 else:
-	print "Pasta nao Existe"
+	print "Pasta nao Existe!"
